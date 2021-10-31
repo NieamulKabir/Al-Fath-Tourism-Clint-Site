941b51e78ff0dbc9b005f0fd98bde8aa1afa804c
NieamulKabir <kabir000079@gmail.com>
2021-10-31 22:51:34 +0600

    added

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdexeJxRhtBCj0ibGmcXJ6HbUff3St4So",
+    authDomain: "alfath-89d51.firebaseapp.com",
+    projectId: "alfath-89d51",
+    storageBucket: "alfath-89d51.appspot.com",
+    messagingSenderId: "940692932765",
+    appId: "1:940692932765:web:ed28588bd35c9eda37ac74"
+  };
+  
+export default firebaseConfig;
